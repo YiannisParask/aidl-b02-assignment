192533682920d74642a85f3b4e65e1739eb6e7aa
YiannisParask <yiannis.parask4@gmail.com>
2025-01-18 22:01:33 +0200

    fixed the agent by adding time steps

--- src/agent.py
+++ src/agent.py
@@ -20,6 +20,7 @@ class Agent:
         epsilon_start=1.0,
         epsilon_end=0.1,
         epsilon_decay=0.995,
+        update_every=4,
     ):
         self.state_size = state_size
         self.action_size = action_size
@@ -29,6 +30,8 @@ class Agent:
         self.epsilon = epsilon_start
         self.epsilon_end = epsilon_end
         self.epsilon_decay = epsilon_decay
+        self.update_every = update_every
+        self.t_step = 0
 
         # Initialize Q-network and target network
         self.q_network = QNetwork(action_size).to(device)
@@ -42,17 +45,18 @@ class Agent:
         # Optimizer
         self.optimizer = optim.Adam(self.q_network.parameters(), lr=lr)
 
-    def act(self, state, training=True):
+    def act(self, state, epsilon=None):
         '''
         Select an action based on the current state.
         
         Args:
             state (np.array): Current state of the environment.
-            training (bool): Whether the agent is training or not.
+            epsilon (float): Exploration rate.
         Returns:
             action (int): Action to take.
         '''
-        if training and random.random() < self.epsilon:
+        epsilon = self.epsilon if epsilon is None else epsilon
+        if random.random() < epsilon:
             return random.randint(0, self.action_size - 1)
         else:
             state_tensor = (
@@ -73,27 +77,39 @@ class Agent:
             done (bool): Whether the episode has ended
         '''
         self.replay_buffer.add((state, action, reward, next_state, done))
-        if len(self.replay_buffer) >= self.replay_buffer.batch_size:
+        
+        self.t_step += 1
+        
+        if self.t_step % self.update_every == 0 and len(self.replay_buffer) >= self.replay_buffer.batch_size:
             self.learn()
+            
         self.epsilon = max(self.epsilon_end, self.epsilon * self.epsilon_decay)
+
 
     def learn(self):
         '''
         Perform a single step of learning.
         '''
         states, actions, rewards, next_states, dones = self.replay_buffer.sample()
-        q_values = self.q_network(states).gather(1, actions.unsqueeze(1)).squeeze()
+        
+        q_values = self.q_network(states).gather(1, actions.unsqueeze(1)).view(-1)
+        
         with torch.no_grad():
             next_q_values = self.target_network(next_states).max(1)[0]
             targets = rewards + (self.gamma * next_q_values * (1 - dones))
+            
 
-        loss = nn.MSELoss()(q_values, targets)
+        loss = nn.SmoothL1Loss()(q_values, targets)
         self.optimizer.zero_grad()
         loss.backward()
+        
         nn.utils.clip_grad_norm_(self.q_network.parameters(), max_norm=1.0)
         self.optimizer.step()
 
         self.soft_update(self.q_network, self.target_network)
+        
+        # Debug loss
+        #print(f"Loss: {loss.item()}")
 
     def soft_update(self, source_model, target_model):
         """
